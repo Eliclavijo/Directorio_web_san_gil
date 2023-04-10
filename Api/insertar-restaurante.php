564c00ccca69bb48cc975e7aba1b30c917cc77a7
Eliclavijo <eli.13.07@hotmail.com>
2023-04-10 18:20:55 -0500

    Avance 3

--- Api/insertar-restaurante.php
+++ Api/insertar-restaurante.php
@@ -1,25 +1,25 @@
 <?php
-// include ("../Conexion/bd.php");
-//     ini_set('display_errors', 1);
-// date_default_timezone_set("America/Bogota");
+include ("../Conexion/bd.php");
+    ini_set('display_errors', 1);
+date_default_timezone_set("America/Bogota");
   
-// $calificacion=$_POST['calificacion'];
-// $comentario=$_POST['comentario'];
-// $cod_producto=$_POST['cod_producto'];
+$calificacion=$_POST['calificacion'];
+$comentario=$_POST['comentario'];
+$cod_producto=$_POST['cod_producto'];
 
-// $query = "insert into comentarios (calificacion, comentario, cod_producto) values ('$calificacion', '$comentario','$cod_producto')";
+$query = "insert into comentarios (calificacion, comentario, cod_producto) values ('$calificacion', '$comentario','$cod_producto')";
 
-// $result = $conn ->query($query);
+$result = $conn ->query($query);
 
-// if($result){
-//     $header = "MIME-Version: 1.0 \r\n";
-//     $header.= "Content-type: text/html; charset=utf-8 \r\n";
-//     mail($header);
-//     $regresar = $_SERVER['HTTP_REFERER'];
-//     echo "<script>document.location='$regresar';</script>";
-// }else{
-//     echo "No se inserto";
-// }
+if($result){
+    $header = "MIME-Version: 1.0 \r\n";
+    $header.= "Content-type: text/html; charset=utf-8 \r\n";
+    mail($header);
+    $regresar = $_SERVER['HTTP_REFERER'];
+    echo "<script>document.location='$regresar';</script>";
+}else{
+    echo "No se inserto";
+}
 
-// mysqli_close($conn);
+mysqli_close($conn);
 ?>
